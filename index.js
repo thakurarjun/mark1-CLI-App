1888d84933679455d9d3f33491179e1dfe0ddd6b
Arjun <thakurarjun880@gmail.com>
2021-12-27 07:58:01 +0530

    used chalk library

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var readlineSync = require("readline-sync");
+const readlineSync = require("readline-sync");
+const chalk = require("chalk");
 
 var score = 0;
 var highScore = {
@@ -6,10 +7,9 @@ var highScore = {
     score: 4
 }
 
-
 var name = readlineSync.question("May I have your name please? ");
 var welcomeMessage = "Welcome " + name;
-console.log(welcomeMessage);
+console.log(chalk.black.bgYellow(welcomeMessage));
 
 var userAge = readlineSync.question("what is your age? ");
 console.log(userAge);
@@ -19,21 +19,21 @@ console.log(userAge);
 function userQuestion(question, answer) {
   var userAnswer = readlineSync.question(question);
   if (userAnswer === answer) {
-    console.log("you are absolutely right!");
+    console.log(chalk.green.bold("you are absolutely right!"));
     score = score + 1;
   } else {
-    console.log("you are wrong");
+    console.log(chalk.red.bold("you are wrong"));
     score = score - 1;
   }
-  console.log("your score: " + score);
+  console.log(chalk.yellow("your score: " + score));
   console.log("---------")
 }
 var questions = [
-  { question: "who is my favourite superhero?", answer: "captain america" },
-  { question: "who is my favourite singer?", answer: "diljit dosanj" },
-  {question: "where do I live?", answer: "himachal pradesh"}, 
-  {question: "where is my hometown?", answer: "mandi"}, 
-  {question: "what is my highest qualificaton?", answer: "msc"}
+  { question: chalk.whiteBright.underline.bgBlue("who is my favourite superhero?"), answer: "captain america" },
+  { question: chalk.whiteBright.underline.bgBlue("who is my favourite singer?"), answer: "diljit dosanj" },
+  {question: chalk.whiteBright.underline.bgBlue("where do I live?"), answer: "himachal pradesh"}, 
+  {question: chalk.whiteBright.underline.bgBlue("where is my hometown?"), answer: "mandi"}, 
+  {question: chalk.whiteBright.underline.bgBlue("what is my highest qualificaton?"), answer: "msc"}
 
 ];
 for (var i = 0; i < questions.length; i++) {
@@ -43,9 +43,9 @@ for (var i = 0; i < questions.length; i++) {
 function gameOver () {
     if (score === questions.length) {
 
-    console.log("you have got highest " + score + " points!")
+    console.log(chalk.green.bold("you have got highest " + score + " points!"))
 } else {
-    console.log("you have got " + score + " points!")
+    console.log(chalk.red.bold("you have got " + score + " points!")) 
 }
 
 }
@@ -53,10 +53,10 @@ gameOver();
  
 function currentUser() {
     if (score >= highScore.score) {
-console.log("Congrats you have beaten high score.Now send me your screenshot.")
+console.log(chalk.blue.bold("Congrats you have beaten high score.Now send me your screenshot. "))
     } else {
-        console.log("Great job ...but you are some points behind")
+        console.log(chalk.red.bold("Great job ...but you are some points behind"))
     }
-    console.log("total score " + score)
+    console.log(chalk.yellow("total score " + score))
 };
 currentUser();
